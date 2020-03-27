bc70a6477dd4b6848f3e270dea670d861bc705f3
Prithvi Raju <nprithviraj24@gmail.com>
2020-03-27 23:30:38 +0700

    dataloader is now flexible

--- CycleGAN-with-EDSR/dataloader.py
+++ CycleGAN-with-EDSR/dataloader.py
@@ -22,38 +22,22 @@ def get_data_loader(image_type,  **kwargs):
     SetRange = transforms.Lambda(lambda X: 2 * X - 1.)
     SetScale = transforms.Lambda(lambda X: X / X.sum(0).expand_as(X))
 
-    # resize and normalize the images
-    transformLR = transforms.Compose([
-        transforms.Resize(kwargs['exp_params']['lr_imageSize']),
-        transforms.ToTensor()])
     # get training and test directories
     # resize and normalize the images
-
-    # train_path = os.path.join(kwargs['exp_params']['img_'], image_type)
-    # test_path = os.path.join(kwargs['exp_params']['image_path'], 'test_{}'.format(image_type))
-
-    # if image_type == 'lr':
-    #     # define datasets using ImageFolder
-    #     train_dataset = datasets.ImageFolder(train_path, transformLR)
-    #     test_dataset = datasets.ImageFolder(test_path, transformLR)
-    #
-    #     # create and return DataLoaders
-    #     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-    #     test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-
-
     if image_type == 'lr':
-        train_path = os.path.join(kwargs['exp_params']['data_path'], '')
+        transformLR = transforms.Compose([
+            transforms.Resize(kwargs['exp_params']['lr_imageSize']),
+            transforms.ToTensor()
+        ])
+
+        path = os.path.join(kwargs['exp_params']['data_path'], kwargs['exp_params']['lr_datapath'])
         # test_path = os.path.join(image_path, 'test_{}'.format(image_type))
         # define datasets using ImageFolder
-        dataset = datasets.ImageFolder(kwargs['exp_params']['lr_datapath'], transformLR)
-        n = len(dataset)
-        # test_dataset = datasets.ImageFolder(test_path, transform2)
+        dataset = datasets.ImageFolder(path, transformLR)
+        split = int(kwargs['exp_params']['test_split'] * len(dataset) / 100)
 
-            # create and return DataLoaders
-        # data_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, drop_last=True)
-        train_set, val_set = torch.utils.data.random_split(dataset, [1600, n-1600])
-
+        # create and return DataLoaders
+        train_set, test_set = torch.utils.data.random_split(dataset, [len(dataset) - split, split])
 
     if image_type == 'hr':
         transformHR = transforms.Compose([
@@ -63,19 +47,20 @@ def get_data_loader(image_type,  **kwargs):
             transforms.Resize(kwargs['exp_params']['hr_imageSize']),
             transforms.ToTensor()
         ])
-        train_path = os.path.join(kwargs['exp_params']['data_path'], '')
+        path = os.path.join(kwargs['exp_params']['data_path'], kwargs['exp_params']['hr_datapath'])
         # test_path = os.path.join(image_path, 'test_{}'.format(image_type))
         # define datasets using ImageFolder
-        dataset = datasets.ImageFolder(kwargs['exp_params']['hr_datapath'], transformLR)
-        n = len(dataset)
-        # test_dataset = datasets.ImageFolder(test_path, transform2)
+        dataset = datasets.ImageFolder(path, transformHR)
+        split = int(kwargs['exp_params']['test_split']*len(dataset)/100)
 
         # create and return DataLoaders
         # data_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, drop_last=True)
-        train_set, val_set = torch.utils.data.random_split(dataset, [1600, n - 1600])
+        train_set, test_set = torch.utils.data.random_split(dataset, [len(dataset)-split, split])
 
-    train_loader = DataLoader(dataset=train_set, batch_size=kwargs['exp_params']['batch_size'], shuffle=True, num_workers=num_workers, drop_last=True)
-    test_loader = DataLoader(dataset=val_set, batch_size=batch_size, num_workers=num_workers, drop_last=True)
+    train_loader = DataLoader(dataset=train_set, batch_size=kwargs['exp_params']['batch_size'],
+                              shuffle=kwargs['exp_params']['shuffle'], num_workers=kwargs['exp_params']['num_workers'], drop_last=True)
+    test_loader = DataLoader(dataset=test_set, batch_size=kwargs['exp_params']['batch_size'],
+                             num_workers=kwargs['exp_params']['num_workers'], drop_last=True)
 
 
     return train_loader, test_loader
